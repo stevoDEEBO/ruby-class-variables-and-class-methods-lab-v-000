85051fcc9f524f36220ec68f80d13574812a5b95
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-10 18:51:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   @@artists = []
 
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << genre
     @@artists << artist
