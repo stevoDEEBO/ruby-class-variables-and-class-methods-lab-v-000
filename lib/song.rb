0f41dad4d8482142808f18f4b0cf54116accf901
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-10 18:23:49 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize
+    @@count += 1
+    @@genres << @genre
+    @@artists << @artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres
+    
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genre_count
+
+  end
+
+  def self.artist_count
+
+  end  
+end
